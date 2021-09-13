51f14e536bac62262950e0fad572138fa1a3b72c
anilarajshekar <anila.talari@gmail.com>
2021-09-13 15:44:49 +0530

    Added a Login Form

--- index.php
+++ index.php
@@ -8,8 +8,40 @@
 </head>
 <body>
     
-<?php
-echo ("Hello World");
-?>
+<div class="uk-section uk-container uk-text center">
+    <div class= "uk-grid uk-child uk-child-width-1-3@s uk-child-width-1-1"> 
+
+    <form action="" class="ul-form-stacked js-login">
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Email</label>
+    <div class="uk-form-controls">
+        <input type="email" class="uk-input" id="form-stacked-text" required="required" placeholder="email@email.com">
+    </div>
+</div>
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Password</label>
+    <div class="uk-form-controls">
+        <input type="password" class="uk-input" id="form-stacked-text" required="required" placeholder="Your Password">
+    </div>
+</div>
+
+<div class="uk-margin">
+    <button class="uk-button uk-button-default" type="submit">Login</button>
+    </div>
+</div>
+
+</form>
+</div>
+</div>
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.7.3/dist/js/uikit-icons.min.js"></script>
+
+
 </body>
 </html>
